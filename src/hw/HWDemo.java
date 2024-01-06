af1ca2a009735c588db8706e9cec8818447e7e9d
haithemalsaffar <155380799+haithemalsaffar@users.noreply.github.com>
2024-01-06 06:05:02 -0500

    Corrected comment typo

--- src/hw/HWDemo.java
+++ src/hw/HWDemo.java
@@ -9,7 +9,7 @@ public class HWDemo {
 		System.out.println("it begins at 9:19 ");//Alex 
 		// if you're seeing this you're late
 		// commented by Haithem at 5:59 on 01/06/2024
-		// Demp Push from Haithem WorkPlace
+		// Demo Push from Haithem WorkPlace
 }
 	} 
 
